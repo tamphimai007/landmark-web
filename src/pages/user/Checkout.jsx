19b090fadf9e21371f46f83ff94c15c3460bba6c
tamisan <phongphat.jap@gmail.com>
2025-02-22 08:47:32 +0700

    update

--- src/pages/user/Checkout.jsx
+++ src/pages/user/Checkout.jsx
@@ -7,9 +7,7 @@ import {
 import { useAuth } from "@clerk/clerk-react";
 import { checkOut } from "@/api/booking";
 import { useParams } from "react-router";
-const stripePromise = loadStripe(
-  "pk_test_51QCvoSCV4BDL9XJusLr3AZKhG8jL529Y3G1LZqvOD3CTt3fy4IISiKgDYkDYtfLf894jCWjD4f26FQJ1Wd668oWE0069v0G1EE"
-);
+const stripePromise = loadStripe("");
 
 const Checkout = () => {
   // Javascript
@@ -17,16 +15,16 @@ const Checkout = () => {
   const { id } = useParams();
 
   const fetchClientSecret = async () => {
-    const token = await getToken()
+    const token = await getToken();
     try {
       const res = await checkOut(token, id);
-      return res.data.clientSecret
+      return res.data.clientSecret;
     } catch (error) {
       console.log(error);
     }
   };
 
-  const options = {fetchClientSecret};
+  const options = { fetchClientSecret };
   return (
     <div id="checkout">
       <EmbeddedCheckoutProvider stripe={stripePromise} options={options}>
